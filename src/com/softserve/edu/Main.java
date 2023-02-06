4de4c8e3f3a3902254124be37d320b654e094cf7
Vladyslav Braha <brahav@ext.inditex.com>
2023-02-06 17:55:37 +0200

    add greeting message

--- src/com/softserve/edu/Main.java
+++ src/com/softserve/edu/Main.java
@@ -31,6 +31,7 @@ public class Main {
         //
         System.out.println("Hello " + name);
         System.out.println("You are " + age);
+        System.out.println("Nice to see you! ");
         //
         sc.close();
     }
