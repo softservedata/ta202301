ba039d40cf732b1242367382e31db34d2931cba3
Yaroslav <garasym@gmail.com>
2023-02-07 09:14:35 +0200

    add a.txt

--- src/com/softserve/edu01/A.java
+++
@@ -1,7 +0,0 @@
-package com.softserve.edu01;
-
-public class A {
-    public void m1() {
-        System.out.println("m1() form edu01.A");
-    }
-}
--- src/com/softserve/edu02/A.java
+++
@@ -1,7 +0,0 @@
-package com.softserve.edu02;
-
-public class A {
-    public void m1() {
-        System.out.println("m1() form edu02.A");
-    }
-}
