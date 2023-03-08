dc8f539f44e07aed166b8b98c509d957471b94a9
Nazariy <nvyntempt@gmail.com>
2023-03-08 13:22:14 +0200

    exclude unused import

--- src/com/softserve/edu/hw8/Main.java
+++ src/com/softserve/edu/hw8/Main.java
@@ -1,15 +1,14 @@
 package com.softserve.edu.hw8;
 import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
     public static void main (String[] args){
-        ArrayList<Integer> myNumbers = new ArrayList<Integer>();
+        ArrayList<Integer> myNumbers = new ArrayList<>();
         myNumbers.add(10);
         myNumbers.add(15);
         myNumbers.add(40);
 
-        ArrayList<Integer> myNumbers2 = new ArrayList<Integer>();
+        ArrayList<Integer> myNumbers2 = new ArrayList<>();
         myNumbers2.add(10);
         myNumbers2.add(15);
         myNumbers2.add(20);
@@ -20,6 +19,5 @@ public class Main {
         myNumbers2.retainAll(myNumbers);  //returns the common elements in both list
         System.out.println("Common elements in lists: ");
         System.out.println(myNumbers2);
-
     }
 }
