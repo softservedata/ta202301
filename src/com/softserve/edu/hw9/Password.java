e45a7b12c967697395c613e532a086c2e73b378f
Vladyslav Braha <brahav@ext.inditex.com>
2023-03-14 22:15:24 +0200

    add hw 9

---
+++ src/com/softserve/edu/hw9/Password.java
@@ -0,0 +1,29 @@
+package com.softserve.edu.hw9;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Password {
+    private static final String PASSWORD_PATTERN =
+            "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[_!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}$";
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean isPasswordCorrect;
+        String result;
+        do {
+            System.out.println("Enter a password: ");
+            String enteredPassword = scanner.nextLine();
+
+            isPasswordCorrect = isCorrectPassword(enteredPassword);
+            result = isPasswordCorrect ? "Password is correct" : "Password is incorrect";
+            System.out.println(result);
+        } while (!isPasswordCorrect);
+    }
+    static boolean isCorrectPassword(String password) {
+        Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
+        Matcher matcher = pattern.matcher(password);
+        return matcher.matches();
+    }
+}
